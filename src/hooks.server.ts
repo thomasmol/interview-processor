3d0c085999dda9612c6fe020d241c154b5ead1cc
Thomas Mol <email@thomasmol.com>
2023-02-06 19:21:19 +0100

    Fixed config and headers

---
+++ src/hooks.server.ts
@@ -0,0 +1,10 @@
+import type { Handle } from '@sveltejs/kit';
+
+export const handle = (async ({ event, resolve }) => {
+	const response = await resolve(event);
+	response.headers.set('Access-Control-Allow-Origin', '*');
+	response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+	response.headers.set('Cross-Origin-Opener-Policy', 'same-origin');
+	response.headers.set('Cross-Origin-Embedder-Policy', 'require-corp');
+	return response;
+}) satisfies Handle;
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-/** @type {import('vite').Plugin} */
-const viteServerConfig = {
-	name: 'log-request-middleware',
-	configureServer(server) {
-		server.middlewares.use((req, res, next) => {
-			res.setHeader('Access-Control-Allow-Origin', '*');
-			res.setHeader('Access-Control-Allow-Methods', 'GET');
-			res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-			res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-			next();
-		});
-	}
-};
+
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit(), viteServerConfig],
+	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
